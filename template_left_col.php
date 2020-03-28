71f28369f45529871e84d31ba55c145932f9736f
cobber <codecobber@gmail.com>
2020-03-28 15:17:32 +0000

    update logo to side name and made new templates

--- template_left_col.php
+++ template_left_col.php
@@ -1,23 +1,22 @@
 <?php include('includes/header.inc.php'); ?>
           <!-- Parallax Header -->
 
+    <div class="pageHeaderGrey">
+      <h1><?php get_page_title();?></h1>
+    </div>
+
     <div class='container' id='start'>
+        <div id="bck_1" class="bck contentBlock">
+           <div id="block_1" class="row rowNo23">
+             <div class="l9 m9 columns">
+               <?php get_page_content();?>
+             </div>
 
-
-      <div id="bck_1" class="bck">
-         <div id="block_1" class="row rowNo08">
-           <div class="l9 m9 columns">
-             <?php get_page_content(); ?>
-           </div>
-         </div>
-      </div>
-
-      <div id="bck_1" class="bck">
-         <div id="block_1" class="row rowNo08">
            <div class="l3 m3 columns">
-             <?php get_page_content(); ?>
+             <?php get_component('sidebar'); ?>
            </div>
          </div>
       </div>
     </div>
-          <?php include('includes/footer.inc.php'); ?>
+
+    <?php include('includes/footer.inc.php'); ?>
