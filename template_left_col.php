3e71d8d78ae4fbda1ee7d39f2a6e0491a64b010f
cobber <codecobber@gmail.com>
2020-03-30 10:58:44 +0100

    updated left column template, css and breadcrumbs for breadcrumbs

--- template_left_col.php
+++ template_left_col.php
@@ -3,11 +3,25 @@
 
     <div class="pageHeaderGrey">
       <h1><?php get_page_title();?></h1>
+
+          <div class="crumbsBG">
+          <div id="bck_1" class="bck contentBlock container">
+             <div id="block_1" class="row rowNo23">
+               <div class="l12 m12 columns">
+                  <div class="breadcrumbs">
+                    <a href="<?php echo find_url('index',null); ?>">Home</a>
+                    <?php get_i18n_breadcrumbs(return_page_slug()); ?>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+
     </div>
 
     <div class='container' id='start'>
-        <div id="bck_1" class="bck contentBlock">
-           <div id="block_1" class="row rowNo23">
+        <div id="bck_2" class="bck contentBlock">
+           <div id="block_2" class="row rowNo23">
              <div class="l9 m9 columns">
                <?php get_page_content();?>
              </div>
